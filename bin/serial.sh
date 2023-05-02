8054cbdfc46249d58c4b3588d9f8c27d96158f54
goncalopata <goncalo.pereira-pata@etu.u-paris.fr>
2023-05-02 22:01:50 +0200

    first commit

---
+++ bin/serial.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BINARY=$1
+HEX=$2
+OUT=$3
+
+ADDR_TIMES=`arm-none-eabi-objdump $BINARY -t | grep bench_times | cut -f 1 -d  ' '`
+
+openocd -l /dev/null \
+        -f /usr/share/openocd/scripts/board/st_nucleo_f4.cfg \
+        &>/dev/null &
+# Get the pid of `openocd` (not `sudo opencd`)
+OPENOCDPID=$!
+
+sleep 1
+
+TMP=`mktemp`
+
+(echo "init; reset halt; flash write_image erase $HEX; reset run; sleep 2000"; echo "mdw 0x$ADDR_TIMES"; echo 'exit') | netcat localhost 4444 > $TMP
+
+grep -a -v '>' $TMP | grep -a "$ADDR_TIMES" | cut -f 2-4 -d ' ' > $OUT
+
+rm $TMP
+
+# Kill openocd
+kill $OPENOCDPID
